9e9d1cadd25f0800c7aa34fc83492773d147941c
diegohc1 <dhermoza.c@gmail.com>
2022-12-23 16:51:54 -0500

    quehacemo

--- 02-reportes-scripts/04-exploraciones1.R
+++ 02-reportes-scripts/04-exploraciones1.R
@@ -12,24 +12,83 @@ devtools::source_url("https://raw.githubusercontent.com/diegohc1/para_funciones/
 lista = rio::import_list(Sys.glob(here("01-data", "04-para-el-analisis", "*.sav")))
 
 bd <- lista$EM2022_2Sestudiante_EBRD2
+bdlm <- filter(bd, !provincia %in% c("HUAROCHIRÍ", "CANTA"))
+
+unique(bd$provincia)
 vv <- names(bd)[startsWith(names(bd), "EST")]
 
-# lmer(ise2S~1 + (1|cod_mod7), data = bd1) %>% calc_icc()
+# lmer(ise2S~1 + (1|cod_mod7), data = bd) %>% calc_icc()
 
-bd %>%
-  select(all_of(vv), ise2S) %>%
-  cor2()
-
-ggplot(bd, aes(y = EST2SMAT_DISRP, x = ise2S)) + 
-  geom_point()
+ggplot(bd, aes(y = EST2SMAT_DISRP, x = ise2S)) + geom_point()
 
 hist(bd$ise2S)
 hist(bd$EST2SMAT_DISRP)
 
-lmer(EST2SGEN_PRAER ~ ise2S + (1|cod_mod7), data = bd) %>% fixef()
+bd %>% select(ise2S, starts_with("EST")) %>% cor2()
 
 map(vv, ~lmer(paste(.x, "~ ise2S + (1|cod_mod7)"), data = bd) %>% fixef())
 
+bd %>%
+  group_by(cod_mod7) %>%
+  select(ise2S, starts_with("EST")) %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  select(-cod_mod7) %>%
+  cor2()
+
+
+padron <- rio::import("D:/1. UMC/2023/00-padron/Padron_web.dbf")
+names(padron)
+padron2 <- select(padron, cod_mod7 = COD_MOD, D_GESTION)
+
+bb3 <- left_join(bdlm, padron2, by = "cod_mod7")
+bb3 <- mutate(bb3, gestion = ifelse(D_GESTION == "Privada", "Privada", "Publica"))
+
+table(bb3$gestion)
+
+bb3$ise2S <- attr(bb3$ise2S, "ISE")
+
+
+
+bb3 %>%
+  group_by(gestion) %>%
+  select(ise2S, starts_with("EST")) %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  pivot_longer(-gestion, names_to = "var", values_to = "est") %>%
+  ggplot(aes(x = var, y = est, color = gestion)) + 
+  geom_point(size = 4)
+
+
+bb3 %>%
+  group_by(cod_mod7) %>%
+  summarise(across(starts_with("EST"), mean, na.rm = TRUE),
+            gestion = first(gestion)) %>%
+  group_by(gestion) %>%
+  summarise(across(starts_with("EST"), mean, na.rm = TRUE)) %>%
+  pivot_longer(-gestion, names_to = "var", values_to = "est") %>%
+  ggplot(aes(x = var, y = est, color = gestion)) + 
+  geom_point(size = 4)
+
+
+# pegado con docente ------ 
+
+bddoc <- lista$EM2022_2SdocenteCOM_EBR
+bddoc <- select(bddoc, cor_minedu, starts_with("DOC"))
+
+bdie <- bd %>%
+  group_by(cod_mod7) %>%
+  summarise(isep = mean(ise2S, na.rm = TRUE),
+            across(starts_with("EST"), mean, na.rm = TRUE),
+            cor_minedu = first(cor_minedu))
+
+df <- left_join(bdie, bddoc, by = "cor_minedu")
+
+# esta bien pegado, mal pegado? para el martes! 
+df %>%
+  select(isep, starts_with("EST"), starts_with("DOC")) %>%
+  cor2()
+
+
+# confiabilidad de variables de nivel 2 reportadas en nivel 1 ----
 
 bd1 <- bd %>%
   select(cod_mod7, EST2SLEC_ACTREFL, EST2SMAT_DISRP, EST2SMAT_ACTCOG, EST2SMAT_EVFORM, ise2S)
@@ -40,31 +99,17 @@ lmer(EST2SMAT_ACTCOG~1 + (1|cod_mod7), data = bd1) %>% calc_icc()
 lmer(EST2SMAT_EVFORM~1 + (1|cod_mod7), data = bd1) %>% calc_icc()
 
 # ICC(1)
-icc1 <- lmer(EST2SMAT_DISRP~1 + (1|cod_mod7), data = bd1) %>% calc_icc()
+icc1 <- lmer(EST2SMAT_EVFORM~1 + (1|cod_mod7), data = bd1) %>% calc_icc()
 
 # ICC(2)
 k <- bd1 %>%
   group_by(cod_mod7) %>%
-  mutate(n = n()) %%
+  mutate(n = n()) %>%
+  pull(n) %>%
+  mean()
 
-k <- mean(bdfinal$n)
 # k*ICC(1) / 1 + (k-1)*ICC
-icc2 <- k*icc1 / (1 + (k - 1)*icc1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(icc2 <- k*icc1 / (1 + (k - 1)*icc1))
 
 
 

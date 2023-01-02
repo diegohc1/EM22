782370418c3e920a5b9087a7363c15f8c7ff3d25
Diego Hermoza <dhermoza.c@gmail.com>
2023-01-02 18:33:07 -0500

    dif sig

--- 03-otros/05-rend-segun-cuartiles-ise.R
+++ 03-otros/05-rend-segun-cuartiles-ise.R
@@ -1,4 +1,7 @@
 
+library(here)
+library(tidyverse)
+
 # rendimiento 
 
 lista_rend = rio::import_list(Sys.glob(here("01-data", "05-bd-rendimiento", "*.sav")), setclass = "tibble") 
@@ -18,8 +21,6 @@ bdrend <- bind_rows(rend, ece19b)
 
 # ise 
 ise <- rio::import(here("01-data", "03-intermedias", "02b-puntajes-ise",  "FFAA-indices2019_2022.sav"))
-
-
 bd <- left_join(bdrend, ise, by = c("año", "cor_minedu", "cor_est"))
 
 
@@ -62,16 +63,71 @@ tab2 %>%
 lista_rend = rio::import_list(Sys.glob(here("01-data", "05-bd-rendimiento", "*.sav")), setclass = "tibble") 
 rend <- lista_rend[[1]]
 
+ise <- rio::import(here("01-data", "03-intermedias", "02b-puntajes-ise",  "FFAA-indices2019_2022.sav"))
+ise <- filter(ise, año == "2022")
+names(ise)
+
+rend <- left_join(rend, ise, by = c("cor_minedu", "cor_est"))
+rend <- rend %>% mutate(iseq = ntile(ise2S_19_22, 4))
+
 # install.packages("survey")
 library(survey)
+options(survey.lonely.psu = "certainty") #opcion de survey
 
-bb <- drop_na(rend, Peso_lectura)
 
-bd4p_svy <- svydesign(data = bb, 
-                      id = ~cod_mod7 + ID_seccion, 
-                      strata = ~Estrato_DRE, 
-                      fpc = ~pikIE + probsec, 
+bd_lec <- drop_na(rend, Peso_lectura)
+bd_lec <- mutate(bd_lec, pikie_lec = 1/Peso_IE_lectura,
+                 prob_seccion_lec = 1/Peso_seccion_lectura)
+
+bd_lec_svy <- svydesign(data = bd_lec, 
+                      id = ~cod_mod7.x + ID_seccion, 
+                      strata = ~Estrato, 
+                      fpc = ~pikie_lec + prob_seccion_lec, 
                       nest = TRUE, 
                       pps = "brewer")
+
+m1 <- svyby(~M500_EM_2S_2022_CT, ~iseq, bd_lec_svy, svymean)
+m1
+
+# install.packages("srvyr")
+library(srvyr)
+
+bd4p_mat_srvyr <- bd_mat %>%
+  as_survey_design(ids = c("cod_mod7", "ID_Seccion"), 
+                   strata = "Estrato_DRE", 
+                   fpc = c("pikIE", "probseccionM"), 
+                   nest = TRUE,
+                   pps = "brewer")
+
+
+# error estandar de una poblacion  ?
+
+tab19 <- filter(bd, año == "2019", !is.na(ise2S_19_22)) %>%
+  mutate(qise = ntile(ise2S_19_22, 4)) %>%
+  group_by(qise) %>%
+  summarise(
+    mean1 = mean(M500_M, na.rm = TRUE),
+    sd1 = sd(M500_M, na.rm = TRUE),
+    n1 = n()
+  )
+
+tab19 <- tab19 %>%
+  mutate(se = sd1/sqrt(n1), año = "2019") %>%
+  select(qise, año, mean1, se)
+
+
+
+m1 <- svyby(~M500_EM_2S_2022_CT, ~iseq, bd_lec_svy, svymean)
+tab22 <- as.data.frame(m1) %>%
+  rename(qise = iseq, mean1 = M500_EM_2S_2022_CT) %>%
+  mutate(año = "2022")
+
+tabfinal <- bind_rows(tab22, tab19)
+
+tabfinal %>%
+  split(list(.$qise)) %>%
+  lapply(., function(x) factorito::dif_sig(x, m = "mean1", se = "se", nom = "año", long = TRUE)) %>%
+  bind_rows(.id = "tipo") %>%
+  separate(col = tipo, into = c("Tipo", "Tipo_medida"))
 
 
